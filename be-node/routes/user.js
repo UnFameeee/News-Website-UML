41f1bc38886622c0d4473f27964cd1884c011b20
Quốc Thắng <quocthang036@gmail.com>
2022-06-29 21:33:19 +0700

    commit clean - final commit

--- be-node/routes/user.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const {body} = require('express-validator')
-
-const userController = require("../controllers/UserController");
-
-const router = express.Router();
-
-// GET /api/user
